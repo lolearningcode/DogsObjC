a26c05743cc59cc0b6a811cc61382c7661d8d99b
Cleo "Lo" Howard <lolearningcode@gmail.com>
2019-05-22 09:23:43 -0600

    Initial Commit

---
+++ DogsObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DogsObjC
+//
+//  Created by Lo Howard on 5/22/19.
+//  Copyright © 2019 Lo Howard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DogsObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DogsObjC
+//
+//  Created by Lo Howard on 5/22/19.
+//  Copyright © 2019 Lo Howard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
